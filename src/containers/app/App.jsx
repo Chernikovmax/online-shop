9029db304c08069c0bb49b5c3befebb080c6e1a4
Maxim <chernik.max@gmail.com>
2019-07-20 20:17:24 +0300

    [ADD] Made final configs.

---
+++ src/containers/app/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HOME, WOMEN, MEN, JEWELRY, SHOES, WOMENS_PRODUCT, MENS_PRODUCT } from '../../routes';
+import './App.css';
+
+export default function App() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          
+        </Switch>
+      </BrowserRouter>
+    );
+}
--- src/routes.js
+++ src/routes.js
@@ -3,3 +3,5 @@ export const WOMEN = '/women';
 export const MEN = '/men';
 export const JEWELRY = '/jewelry';
 export const SHOES = '/shoes';
+export const WOMENS_PRODUCT = "/women/:productId";
+export const MENS_PRODUCT = "/men/:productId";
