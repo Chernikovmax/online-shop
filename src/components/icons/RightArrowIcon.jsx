0437d8243a50b8a511e338575ae477a954c6331f
Maxim <chernik.max@gmail.com>
2019-08-09 17:47:30 +0300

    [ADD] slider with it components and styles for it, also added animation for its content.

---
+++ src/components/icons/RightArrowIcon.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const RightArrowIcon = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "21px",
+        height: "21px"
+      }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        x="0"
+        y="0"
+        viewBox="0 0 199.404 199.404"
+        style={{ width: "15px", height: "15px", fill: "#ffffffa3" }}
+      >
+        <path d="M72.166 200.24l97.26-102.087L67.34.893 39.748 29.855l73.135 69.668-69.678 73.125z" />
+      </svg>
+    </div>
+  );
+};
--- src/components/topSlider/TopSlider.jsx
+++ src/components/topSlider/TopSlider.jsx
@@ -1,60 +1,116 @@
-import React, { Component } from 'react';
-import './TopSlider.css';
+import React, { Component } from "react";
+import { RightArrowIcon, LeftArrowIcon } from "../icons";
+import cx from "classnames";
+import "./TopSlider.css";
 
 export class TopSlider extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+  state = {
+    slidesCount: this.props.products.length,
+    activeSlide: 0
+  };
 
-        };
-    }
+  showSlides = () => {
+    const { slidesCount, activeSlide } = this.state;
+    console.log(slidesCount, activeSlide);
 
-    parallax = (event) => {
-        
-        this.styles.transform = `translate(${event.clientX/30}px, ${event.clientY/30}px)`;
-    }
+    let currentSlide = activeSlide;
+    let slidesQuantity = slidesCount;
+    slidesQuantity--;
+    let nextSlide = currentSlide++ < slidesQuantity ? currentSlide++ : 0;
+    this.setState({
+      ...this.state,
+      activeSlide: nextSlide
+    });
+  };
 
-    render() {
-        const { products } = this.props;
-        return (
-            <div className="top-slider">
-                { 
-                    products.map(product => {
-                        const { 
-                            productTitle, productName, productDescription, productCost, imageURL 
-                        } = product;
-                        return (
-                            <div key={imageURL} className="slide">
-                                <img src={imageURL} alt={productName}
-                                    className="slide__background"
-                                    onMouseMove={this.parallax}
-                                />
-                                <div className="slide__content">
-                                    <span className="slide__product-title">
-                                        {productTitle.toUpperCase()}
-                                    </span>
-                                    <span className="slide__product-name">
-                                        {productName.toUpperCase()}
-                                    </span>
-                                    <p className="slide__product-description">
-                                        {productDescription}
-                                    </p>
-                                    <div className="slide__buttons">
-                                        <button className="slide__btn">DISCOVER</button>
-                                        <button className="slide__btn slide__btn-buy">ADD TO CARD</button>
-                                    </div>
+  parallax = event => {
+    // this.styles.transform = `translate(${event.clientX /
+    //   30}px, ${event.clientY / 30}px)`;
+  };
 
-                                </div>
-                                <div className="slide__price-block">
-                                    <span>from</span>
-                                    <span className="slide__price">{productCost}</span>
-                                    <span>SHOP NOW</span>
-                                </div>
-                            </div>
-                        );
-                    })
-                }
+  componentDidMount() {
+    setInterval(this.showSlides, 4000);
+  }
+  render() {
+    const { products } = this.props;
+    const { activeSlide, slidesCount } = this.state;
+    let currentSlide = activeSlide;
+
+    return (
+      <div className="top-slider">
+        {products.map((product, index) => {
+          const {
+            productTitle,
+            productName,
+            productDescription,
+            productCost,
+            imageURL
+          } = product;
+          return (
+            <div
+              key={imageURL}
+              className={cx(
+                "slide",
+                this.state.activeSlide === index && "slide--active"
+              )}
+            >
+              <img
+                src={imageURL}
+                alt={productName}
+                className="slide__background"
+                onMouseMove={this.parallax}
+              />
+              <div className="slide__content">
+                <span className="slide__product-title">
+                  {productTitle.toUpperCase()}
+                </span>
+                <span className="slide__product-name">
+                  {productName.toUpperCase()}
+                </span>
+                <p className="slide__product-description">
+                  {productDescription}
+                </p>
+                <div className="slide__buttons">
+                  <button className="slide__btn">DISCOVER</button>
+                  <button className="slide__btn slide__btn-buy">
+                    ADD TO CARD
+                  </button>
+                </div>
+              </div>
+              <div className="slide__price-block">
+                <div className="price-block__wrapper">
+                  <span>from</span>
+                  <span className="slide__price">{productCost}</span>
+                  <span>SHOP NOW</span>
+                </div>
+              </div>
             </div>
-        );
-    }
+          );
+        })}
+        <div className="slider__footer">
+          <div className="top-slider__togglers">
+            <button className="top-slider__togglers-btn">
+              <LeftArrowIcon />
+            </button>
+            <div
+              className={cx(
+                "top-slider__togglers-dots",
+                activeSlide === 0 && "toggler-dot--active"
+              )}
+            />
+            <div
+              className={cx(
+                "top-slider__togglers-dots",
+                activeSlide === 1 && "toggler-dot--active"
+              )}
+            />
+            <button className="top-slider__togglers-btn">
+              <RightArrowIcon />
+            </button>
+          </div>
+          <div className="slider__counter">{`${++currentSlide} / ${slidesCount}`}</div>
+        </div>
+      </div>
+    );
+  }
 }
