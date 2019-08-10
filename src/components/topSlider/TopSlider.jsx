888cccf1a30084eaf988c8664ad431ada20632a4
Maxim <chernik.max@gmail.com>
2019-08-10 14:34:36 +0300

    [FIX] removed unnecessary import

--- src/components/topSlider/TopSlider.jsx
+++ src/components/topSlider/TopSlider.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { RightArrowIcon, LeftArrowIcon } from "../icons";
 import cx from "classnames";
-import styled from "styled-components";
 import { Background } from "./";
 import "./TopSlider.css";
 
