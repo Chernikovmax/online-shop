d45434021047b007359c8132e5ae36fc0b35c071
Maxim <chernik.max@gmail.com>
2019-08-11 19:42:42 +0300

    [MOD] Style for LikeButton is now made in it's .jsx file

--- src/components/buttons/LikeButton.jsx
+++ src/components/buttons/LikeButton.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { EmptyHeartIcon, FilledHeartIcon } from "../icons";
-import "./LikeButton.css";
 
 export class LikeButton extends Component {
   state = {
@@ -15,9 +14,20 @@ export class LikeButton extends Component {
   };
 
   render() {
+    const styles = {
+      border: "none",
+      borderRadius: "30px",
+      width: "36px",
+      height: "36px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      cursor: "pointer",
+      backgroundColor: "#fff"
+    };
     const { isLiked } = this.state;
     return (
-      <button className="like-btn" onClick={this.toggleLike}>
+      <button style={styles} onClick={this.toggleLike}>
         {isLiked ? <FilledHeartIcon /> : <EmptyHeartIcon />}
       </button>
     );
