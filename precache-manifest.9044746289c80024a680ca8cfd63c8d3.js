2c65ad1786a7f3076c4919efb96924b5df831015
Chernikovmax <chernik.max@gmail.com>
2019-08-14 20:57:00 +0300

    Updates

---
+++ precache-manifest.9044746289c80024a680ca8cfd63c8d3.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0abedabbe8b002698e4564af22620467",
+    "url": "/ghp-online-shop/index.html"
+  },
+  {
+    "revision": "1b82e4131995dd4d766c",
+    "url": "/ghp-online-shop/static/css/main.5216b7db.chunk.css"
+  },
+  {
+    "revision": "bcd90a09753447f0730d",
+    "url": "/ghp-online-shop/static/js/2.2deb7689.chunk.js"
+  },
+  {
+    "revision": "1b82e4131995dd4d766c",
+    "url": "/ghp-online-shop/static/js/main.3c2fa4eb.chunk.js"
+  },
+  {
+    "revision": "8b088f5a00e84b20ffb2",
+    "url": "/ghp-online-shop/static/js/runtime~main.3de78542.js"
+  },
+  {
+    "revision": "ffdd8f73526d0c2a0442e0839c307b30",
+    "url": "/ghp-online-shop/static/media/1.ffdd8f73.jpg"
+  },
+  {
+    "revision": "2e8261f40628c52c60486d6ff9b02782",
+    "url": "/ghp-online-shop/static/media/10.2e8261f4.jpg"
+  },
+  {
+    "revision": "63e4b921830b3d1cfc7028405cc864d1",
+    "url": "/ghp-online-shop/static/media/11.63e4b921.jpg"
+  },
+  {
+    "revision": "17f95c5ae35c66e71ff7c2a66f9211eb",
+    "url": "/ghp-online-shop/static/media/12.17f95c5a.jpg"
+  },
+  {
+    "revision": "cb1db45ee1f69dc50ec5967d4c4c0a30",
+    "url": "/ghp-online-shop/static/media/2.cb1db45e.jpg"
+  },
+  {
+    "revision": "b51ce0bcb32e04df3abe4a8061929852",
+    "url": "/ghp-online-shop/static/media/3.b51ce0bc.jpg"
+  },
+  {
+    "revision": "af9fb444baaa9ac527bca15383caacac",
+    "url": "/ghp-online-shop/static/media/4.af9fb444.jpg"
+  },
+  {
+    "revision": "d60c21cb12cfa70c8d16e3a005548e12",
+    "url": "/ghp-online-shop/static/media/5.d60c21cb.jpg"
+  },
+  {
+    "revision": "36a6f3e00161e826143d0d9d31f808cd",
+    "url": "/ghp-online-shop/static/media/6.36a6f3e0.jpg"
+  },
+  {
+    "revision": "e48f463aad5a1e5e522994b8287c2d83",
+    "url": "/ghp-online-shop/static/media/7.e48f463a.jpg"
+  },
+  {
+    "revision": "40011397233f41b47931ce32bc2dd2f8",
+    "url": "/ghp-online-shop/static/media/8.40011397.jpg"
+  },
+  {
+    "revision": "bc5fba4983fba15692c2b3f2b64697a2",
+    "url": "/ghp-online-shop/static/media/9.bc5fba49.jpg"
+  },
+  {
+    "revision": "e92c285f4a4a7f8f6e2fee84acb0656f",
+    "url": "/ghp-online-shop/static/media/banner.e92c285f.jpg"
+  },
+  {
+    "revision": "3da1a051e4a6e7d8f1af201079e46b32",
+    "url": "/ghp-online-shop/static/media/mini-pic2.3da1a051.png"
+  },
+  {
+    "revision": "c56a6acce7295167e3a552c688dbab31",
+    "url": "/ghp-online-shop/static/media/woman1.c56a6acc.jpg"
+  },
+  {
+    "revision": "135cf584196b4caa623ba02aa82472be",
+    "url": "/ghp-online-shop/static/media/woman2.135cf584.jpg"
+  }
+]);
